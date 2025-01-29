8ececc3a599bc0b3355e6dabdb67422404f10d43
tommasoaricci <ariccitommaso@hotmail.com>
2025-01-29 19:18:07 +0100

    app

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const todoRoutes = require('./src/routes/todoRoute');
 const thoughtRoutes = require('./src/routes/thoughtRoute');
 const userRoutes = require('./src/routes/userRoute');
 const songRoutes = require('./src/routes/songRoute');
+const PORT = process.env.PORT || 4000;
+const connectDB = require('./database');
 
 app.use(express.json());
 
@@ -27,5 +29,11 @@ app.use("/api", todoRoutes, thoughtRoutes, userRoutes, songRoutes);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
 
 module.exports = app;
