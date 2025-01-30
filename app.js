16eead9245e731f3323dc85ed59f46a1aef01c98
tommasoaricci <ariccitommaso@hotmail.com>
2025-01-30 21:01:08 +0100

    build

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use("/api", todoRoutes, thoughtRoutes, userRoutes, songRoutes);
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 connectDB();
